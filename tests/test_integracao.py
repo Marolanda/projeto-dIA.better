814d76eef1ce2856fa29800cb914cc1f753e1648
Júlio César <jcsouzai418@gmail.com>
2024-11-26 14:35:09 -0300

    Fazendo alterações no código.

--- tests/test_integracao.py
+++ tests/test_integracao.py
@@ -1,6 +1,5 @@
 from app import app
 
-#verifica se a rota / (página inicial) está funcionando corretamente.
 def test_home_route():
     client = app.test_client()
     response = client.get('/')
@@ -8,14 +7,12 @@ def test_home_route():
     assert "Previsão de Diabetes" in response.data.decode('utf-8')
     assert b"Seu IMC" in response.data
 
-#valida o cálculo de IMC (Índice de Massa Corporal) na rota /imc.
 def test_imc_calculation():
     client = app.test_client()
     response = client.post('/imc', data={'altura': '1.70', 'peso': '70'})
     assert response.status_code == 200
     assert b"Resultado:" in response.data
 
-#verifica a funcionalidade de previsão de diabetes na rota /predict.
 def test_prediction():
     client = app.test_client()
 
@@ -30,7 +27,7 @@ def test_prediction():
         'heart_disease': '0'
     }
 
-    response = client.post('/predict', json=data)  # Envia os dados como JSON
+    response = client.post('/predict', json=data)
     print(response.status_code)
     print(response.data)
     assert response.status_code == 200
--- utils/imc_functions.py
+++ utils/imc_functions.py
@@ -2,19 +2,15 @@
 import re
 
 def validar_peso(peso):
-    # Verifica se o peso é uma string e tenta converter para número
     if isinstance(peso, str):
-        peso = peso.replace(',', '.')  # Substitui a vírgula por ponto
+        peso = peso.replace(',', '.')  
 
-    # Verifica se o peso é um número ou se a string corresponde ao padrão de número com vírgula ou ponto decimal
     regex = r'^[0-9]+([,.][0-9]+)?$'
     if not isinstance(peso, (int, float)) and not re.match(regex, str(peso)):
         raise ValueError("Por favor, insira valores válidos.")
 
-    # Converte a string para número, caso necessário
     peso = float(peso)
 
-    # Verifica se o peso é negativo
     if peso <= 0:
         return False
 
@@ -22,19 +18,15 @@ def validar_peso(peso):
 
 
 def validar_altura(altura):
-    # Verifica se a altura é uma string e tenta converter para número
     if isinstance(altura, str):
-        altura = altura.replace(',', '.')  # Substitui a vírgula por ponto
+        altura = altura.replace(',', '.') 
 
-    # Verifica se a altura é um número ou se a string corresponde ao padrão de número com vírgula ou ponto decimal
     regex = r'^[0-9]+([,.][0-9]+)?$'
     if not isinstance(altura, (int, float)) and not re.match(regex, str(altura)):
         raise ValueError("Por favor, insira valores válidos.")
 
-    # Converte a string para número, caso necessário
     altura = float(altura)
 
-    # Verifica se a altura é negativa
     if altura <= 0:
         return False
 
